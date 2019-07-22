25639528e20251402530513ad04f4a08e7881760
mc2570 <2456524599@qq.com>
2019-07-22 15:56:21 +0800

    444

--- doc/src/main/java/vom/lc/test/Temp.java
+++ doc/src/main/java/vom/lc/test/Temp.java
@@ -4,5 +4,6 @@ public class Temp {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("333");
+        System.out.println("444");
     }
 }
